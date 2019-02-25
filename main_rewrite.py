25e4c430904a126f1c8d1208558771cdfd828e65
Elle Kiernan <elle@1x-nat-vl932-172-30-226-156.wireless.umass.edu>
2019-02-25 12:00:18 -0500

    Game running with a sprite loaded

--- main_rewrite.py
+++ main_rewrite.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import time
 import os, sys
+from pygame.locals import *
 
 #TODO Define classes for each object with sprites
 #TODO 
@@ -29,7 +30,7 @@ def load_images(path):
     for png in image_directory:
         image = pygame.image.load(path + png).convert_alpha()
         image = pygame.transform.scale(image, (128, 256))
-        images.append(png)
+        images.append(image)
         #Make images auto scale to correct size
     return images
 
@@ -42,36 +43,25 @@ class HorseDrugs():
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, width, images, speed):
-        super().__init__(self)
+        super().__init__(all_sprites)
         self.images = images
         self.image = images[0]
-        self.rect = self.image.get_rect(center = (display_width * 0.5))
+        print(type(images[0]))
+        self.rect = self.image.get_rect()
         self.width = width
         self.rect.x =  0.5 * display_width
         self.rect.y = 0.7 * display_height
 
-    def update():
+    def update(self):
         pass
 
 
-path = "/Users/elle/wagongame/"
+path = "/Users/elle/repositories/wagon-repo/images/"
 player_images = load_images(path + "wagon/")
 all_sprites = pygame.sprite.RenderUpdates()
 player_group = pygame.sprite.RenderUpdates()
 player = Player(0.5 * display_width, 0.7 * display_height, 50, player_images, 10)
 player.add(player_group, all_sprites)
-
-
-
-
-def load_images(path):
-    images = []
-    for png in os.listdir(path):
-        image = pygame.image.load(png).convert_alpha()
-        size = pygame.image.get_rect(image)
-        image = pygame.transform.scale(image, display_width * 0.005 * size[0], display_height * 0.005 * size[1])
-        images.append(png)
-    return images
 
 def gameLoop():
 
@@ -83,7 +73,7 @@ def gameLoop():
         if pygame.event.peek(pygame.QUIT) == True:
             gameExit = True
 
-        pygame.display.update(all_sprites.draw())
+        pygame.display.update(all_sprites.draw(gameDisplay))
         clock.tick(30)
 
 
