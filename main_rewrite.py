22d3a4a077a17995572737281a93bd04455a6e45
Elle Kiernan <47838809+ellekiernan@users.noreply.github.com>
2019-02-25 15:05:29 -0500

    horsedrugs sprite created and moving, still need to clear out drawing from old sprites

--- main_rewrite.py
+++ main_rewrite.py
@@ -11,7 +11,7 @@ display_width = 800
 display_height = 600
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 clock = pygame.time.Clock()
-
+image_path = "/Users/elle/repositories/wagon-repo/images/"
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (200, 0, 0)
@@ -35,17 +35,18 @@ class Cactus():
     pass
 
 class HorseDrugs(pygame.sprite.Sprite):
-    def __init__(self, x = random.randrange(0, display_width), y = -300, width = 50, images = [], speed = 10):
+    def __init__(self, x = random.randrange(0, display_width), y = -300, width = 50, images = [], speed = 5):
         super().__init__(all_sprites)
         self.images = images
-        self.image = image[0]
-        self.rect = image.get_rect()
+        self.image = images[0]
+        self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.width = width
         self.speed = speed
 
     def update(self):
+        self.rect.y += self.speed
         pass
     
     def move(self):
@@ -82,33 +83,23 @@ class Player(pygame.sprite.Sprite):
         self.rect.x += self.x_change
         self.rect.y += self.y_change
 
-    # def movePlayer(self):
-    #     pressed = pygame.key.get_pressed()
 
-    #     up, down, left, right = [pressed[key] for key in (pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT)]
-    #     if up:
-    #         self.y_change = -4
-    #     elif down:
-    #         self.y_change = 10
-    #     else:
-    #         self.y_change = 0
-    #     if right:
-    #         self.x_change = 8
-    #     elif left:
-    #         self.x_change = -8
-    #     else:
-    #         self.x_change = 0
-        
-    #     self.rect.x += self.x_change
-    #     self.rect.y += self.y_change
+#load images for things in gameLoop
+player_images = load_images(image_path + "wagon/")
+drug_images = load_images(image_path + "GOODhorsedrugs/")
 
-
-path = "/Users/elle/repositories/wagon-repo/images/"
-player_images = load_images(path + "wagon/")
-all_sprites = pygame.sprite.RenderUpdates()
+#create sprite groups for sprites in gameLoop
 player_group = pygame.sprite.RenderUpdates()
+cactus_group = pygame.sprite.RenderUpdates()
+drug_group = pygame.sprite.RenderUpdates()
+all_sprites = pygame.sprite.RenderUpdates()
+
+#initialize the sprites
 player = Player(0.5 * display_width, 0.7 * display_height, 50, player_images, 10)
-player.add(player_group, all_sprites)
+horse_drugs = HorseDrugs(images = drug_images)
+
+#add sprites to non all_sprites groups (sprites initialized in super().init() to be in all_sprites)
+player.add(player_group)
 
 def gameLoop():
 
